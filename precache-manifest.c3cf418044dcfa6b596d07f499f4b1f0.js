9ec406c869d48bee14f61b41ae70cc6049409593
Rakssz01 <esaalam55@gmail.com>
2020-07-29 15:20:49 +0600

    Updates

---
+++ precache-manifest.c3cf418044dcfa6b596d07f499f4b1f0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197b4e8a9a649e1ddeb881a0d435bb8a",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "0cde38793d8825a65744",
+    "url": "/my-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "786115f5b03ca7864437",
+    "url": "/my-portfolio/static/css/main.5a9abad4.chunk.css"
+  },
+  {
+    "revision": "0cde38793d8825a65744",
+    "url": "/my-portfolio/static/js/2.6df827bc.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/my-portfolio/static/js/2.6df827bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786115f5b03ca7864437",
+    "url": "/my-portfolio/static/js/main.d51b81ef.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  },
+  {
+    "revision": "d4417e9635641fa0fd4e48742caaaae4",
+    "url": "/my-portfolio/static/media/myimg.d4417e96.jpg"
+  },
+  {
+    "revision": "a82133a16e78bae323fd01974de658e9",
+    "url": "/my-portfolio/static/media/portfolio-1.a82133a1.jpg"
+  },
+  {
+    "revision": "5912919eb3290b9e8a98c73dd3d5bc45",
+    "url": "/my-portfolio/static/media/portfolio-2.5912919e.jpg"
+  },
+  {
+    "revision": "fc64f4276a162fca55edbf4eb508184d",
+    "url": "/my-portfolio/static/media/portfolio-3.fc64f427.jpg"
+  },
+  {
+    "revision": "0ad30b5d8140942cb056dd4f3752456c",
+    "url": "/my-portfolio/static/media/portfolio-4.0ad30b5d.jpg"
+  }
+]);
